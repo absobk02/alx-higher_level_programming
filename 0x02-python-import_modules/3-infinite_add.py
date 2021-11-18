a9fb052e8d2c20cb99c58d70387d750e7b721d9b
absobk02 <absobk02@gmail.com>
2021-11-18 14:53:58 +0000

    executable

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-import sys
-
-if __name__ == '__main__':
-    argv = sys.argv[1:]
-
-    res = 0
-    for i in argv:
-        res = res + int(i)
-
-    print(res)
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{}".format(result))
