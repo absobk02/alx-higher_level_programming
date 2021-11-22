ed8f39534054027271f781b61d0496a59ce67047
absobk02 <absobk02@gmail.com>
2021-11-22 21:45:33 +0000

    check no-c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+
+
 def no_c(my_string):
-    new = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new += i
-    return new
+    lst_str = list(my_string)
+    while ('c' in lst_str or 'C' in lst_str):
+        if 'c' in lst_str:
+            lst_str.remove('c')
+        if 'C' in lst_str:
+            lst_str.remove('C')
+
+    return ("".join(lst_str))
